efd0bf109f24eafa3649234cfbb6a453b906c636
Maria Clara Bacelar <claraluciene2005@gmail.com>
2025-09-08 21:57:57 -0300

    Classe Main.java - OK

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner leitorDeTeclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opcaoDoUsuario = 0;
+        System.out.println("Bem-vindo à sua Lista de Tarefas Pessoal!");
+
+        while (opcaoDoUsuario != 5) {
+            System.out.println("\n--- ToDoList - Menu Principal ---");
+            System.out.println("1. Adicionar Tarefa");
+            System.out.println("2. Listar Todas as Tarefas");
+            System.out.println("3. Atualizar Tarefa");
+            System.out.println("4. Deletar Tarefa");
+            System.out.println("5. Marcar Tarefa como Concluída");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcaoDoUsuario = leitorDeTeclado.nextInt();
+            leitorDeTeclado.nextLine();
+
+            switch (opcaoDoUsuario) {
+                case 1:
+                    // Função de adição de tarefa
+                    break;
+                case 2:
+                    // Função de listar tarefas
+                    break;
+                case 3:
+                    // Função de marcar como concluída
+                    break;
+                case 4:
+                    // Função de remover tarefa
+                    break;
+                case 5:
+                    System.out.println("Até a próxima! Mantenha a organização!");
+                    break;
+                default:
+                    System.out.println("Opção inválida, por favor, escolha um número do menu.");
+                    break;
+            }
         }
     }
 }
